20730e3b3595756859c83d4aafac4bdeea445c20
claudiajay <claudiajoppa@gmail.com>
2025-08-01 13:51:45 +0000

    file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import MakeRequest from "./pages/MakeRequest";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/make-request' element={<MakeRequest />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
