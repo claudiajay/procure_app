bfed56f6f7d268afe3fd67521611e959547bdf20
claudiajay <claudiajoppa@gmail.com>
2025-08-15 14:18:17 +0000

    update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ Sentry.setupExpressErrorHandler(app);
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5176', 
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
