55d18e040e8f625ab84e767d3bf3cc4c544ae6cd
claudiajay <claudiajoppa@gmail.com>
2025-08-20 09:47:36 +0000

    cors update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['*'],
-  credentials: true,
 }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
